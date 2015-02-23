3d9a242ae2e2b2cf29e469d39eb2c28399e3a6c6
Nicolas Grenié <nicolas.grenie@gmail.com>
2015-02-23 15:48:42 +0100

    Initial Commit

---
+++ localHemp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  localHemp
+//
+//  Created by Nicolas Grenie on 2/23/15.
+//  Copyright (c) 2015 Nicolas Grenie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
